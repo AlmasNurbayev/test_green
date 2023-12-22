39411c82e492a3e707060e61431bdb7c5e8625bc
almas <ntldr@mail.ru>
2023-12-22 14:37:14 +0600

    add config module, validator, transformer

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,36 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+
+  SwaggerModule.setup(
+    'api',
+    app,
+    SwaggerModule.createDocument(
+      app,
+      new DocumentBuilder()
+        .setTitle(configService.get('swagger.title'))
+        .setDescription(configService.get('swagger.description'))
+        .setVersion(configService.get('swagger.version'))
+        .build(),
+    ),
+  );
+
+  app.useGlobalPipes(
+    // new StripEmptyStringsPipe(),
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      skipMissingProperties: true,
+    }),
+  );
+  console.log(configService.get('port'));
+
+  await app.listen(configService.get('port'));
 }
 bootstrap();
